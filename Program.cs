097a1f8c3e4b1b52658960bd420a70d738567203
IkarosSQ <federleyj@gmail.com>
2023-09-04 13:51:48 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using system;
+
+namespace List
+{
+    class Program
+    {
+        static void main(string[] args) 
+        {
+            console.writeline("hello world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (Console.ReadKey().Key == ConsoleKey.Y) {
+                Console.Beep(2000, 900);
+                Console.Beep(2000, 900);
+                Console.Beep(2000, 900);
+                Console.Beep(2000, 2000);
+            } else if (Console.ReadKey().Key != ConsoleKey.Y) {
+                Console.Beep(5000, 1000);
+            }           
+
+
+
+        }
+
+    }
+}
